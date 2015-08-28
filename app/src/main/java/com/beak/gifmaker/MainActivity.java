9710fc3a334b1b3edeb39f6b06c8c1f41a6cdfdd
boybeak <boybeak@gmail.com>
2015-08-28 17:35:07 +0800

    add log textview

--- app/src/main/java/com/beak/gifmaker/MainActivity.java
+++ app/src/main/java/com/beak/gifmaker/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.beak.gifmakerlib.GifMaker;
@@ -19,34 +22,62 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private Button mMainBtn;
+    private TextView mLogTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Thread() {
+        mMainBtn = (Button)findViewById(R.id.main_start_btn);
+        mLogTv = (TextView)findViewById(R.id.main_log_tv);
+
+        mMainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                super.run();
-                File file = new File (Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Movies" + File.separator + "0.gif");
-                Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.kof_13);
-                GifMaker maker = new GifMaker(2);
-                maker.setOnGifListener(new GifMaker.OnGifListener() {
-                    @Override
-                    public void onMake(int current, int total) {
-                        Log.v(TAG, "make gif current=" + current + " total=" + total);
-                    }
-                });
-                final boolean success = maker.makeGifFromVideo(MainActivity.this, uri, 0, 10 * 1000, 200, file.getAbsolutePath());
-                Log.v(TAG, "make gif success ? " + success + " at:" + file.getAbsolutePath());
-                new Handler(Looper.getMainLooper()).post(new Runnable() {
+            public void onClick(View v) {
+                new Thread() {
                     @Override
                     public void run() {
-                        Toast.makeText(MainActivity.this, "make gif success ? " + success, Toast.LENGTH_SHORT).show();
+                        super.run();
+                        final File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Movies" + File.separator + "0.gif");
+                        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.kof_13);
+                        GifMaker maker = new GifMaker(2);
+                        final Handler handler = new Handler(Looper.getMainLooper());
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Log.v(TAG, "make gif start...data source res/raw/kof_13.mp4");
+                                mLogTv.append("make gif start...data source res/raw/kof_13.mp4" + "\n");
+                            }
+                        });
+                        maker.setOnGifListener(new GifMaker.OnGifListener() {
+                            @Override
+                            public void onMake(final int current, final int total) {
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mLogTv.append("make gif current=" + current + " total=" + total + "\n");
+                                        Log.v(TAG, "make gif current=" + current + " total=" + total);
+                                    }
+                                });
+                            }
+                        });
+                        final long start = System.currentTimeMillis();
+                        final boolean success = maker.makeGifFromVideo(MainActivity.this, uri, 0, 10 * 1000, 200, file.getAbsolutePath());
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "make gif success ? " + success, Toast.LENGTH_SHORT).show();
+                                final long now = System.currentTimeMillis();
+                                mLogTv.append("make gif success ? " + success + " at:" + file.getAbsolutePath() + " cost time:" + ((now - start)/1000) + "s" + "\n");
+                                Log.v(TAG, "make gif success ? " + success + " at:" + file.getAbsolutePath() + " cost time:" + ((now - start)/1000) + "s");
+                            }
+                        });
                     }
-                });
+                }.start();
             }
-        }.start();
+        });
 
     }
 
